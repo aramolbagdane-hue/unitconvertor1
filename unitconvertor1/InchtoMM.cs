0b318440d37458b8869f0b6b7c7fecfca090e9cd
amol <amol@LAPTOP-7KOHTDQM>
2026-02-18 10:41:20 +0530

    Add project files.

---
+++ unitconvertor1/InchtoMM.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace unitconvertor1
+{
+    public class InchtoMM : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the InchtoMM class.
+        /// </summary>
+        public InchtoMM()
+          : base("InchtoMM", "In->MM",
+              "Converts inches into MM",
+              "UnitConvertor1", "Utilities")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Inches", "In", "Value in Inches", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Millimeters", "MM", "Converted value in millimeters", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            double inch = 0.0;
+
+            if (!DA.GetData(0, ref inch))
+                return;
+
+            double mm = inch*25.4;
+
+            DA.SetData(0, mm);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("6CD10A5A-0177-45FC-ADA7-CB30B3BA2B7C"); }
+        }
+    }
+}
---
+++ unitconvertor1/unitconvertor1Info.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper;
+using Grasshopper.Kernel;
+
+namespace unitconvertor1
+{
+    public class unitconvertor1Info : GH_AssemblyInfo
+    {
+        public override string Name => "unitconvertor1";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("24d40ec5-0c6e-43e4-800d-3f90c730f122");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+
+        //Return a string representing the version.  This returns the same version as the assembly.
+        public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
+    }
+}
